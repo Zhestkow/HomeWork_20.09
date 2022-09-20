89f3bb29834535b5fc5a30bce89ed74aeebc03cd
Your Name <you@example.com>
2022-09-20 23:32:16 +0300

    HomeWork_20.09

---
+++ task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите трехзначное число: ");
+int num=Convert.ToInt32(ReadLine());
+int num1 = num/10%10;
+WriteLine($"второе число числа {num} это {num1}");
---
+++ task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int num = Convert.ToInt32(ReadLine());
+int n =10;
+int i=1;
+if (num < 100)
+{
+     WriteLine("нет третьего числа");
+     return;
+}
+while (num%n!=num)
+{
+n=n*10;
+i=i+1;
+}
+WriteLine($"третье число = {(num/(n/1000))%10}");
+
